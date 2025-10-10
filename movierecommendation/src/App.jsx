547b1910cde02ed132c4dcbe4411e4cd4994c9fa
rakinu21 <ralphkinethugdamin@gmail.com>
2025-10-10 11:24:33 +0800

    initial moviecard

--- movierecommendation/src/App.jsx
+++ movierecommendation/src/App.jsx
@@ -1,12 +1,12 @@
 
 import './App.css'
-
+import { MovieCards } from './components/MovieCards'
 function App() {
 
 
   return (
      <div>
-      
+          <MovieCards movie={{title:"kineth", release_date:"2025", url:"",}}/>
      </div>
   )
 }
