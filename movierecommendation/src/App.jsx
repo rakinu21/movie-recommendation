453d8d4a6e8bb162950e9c2cb1f8bfb0977f6b51
rakinu21 <ralphkinethugdamin@gmail.com>
2025-10-10 13:38:02 +0800

    adding router dom and create navbar jsx

--- movierecommendation/src/App.jsx
+++ movierecommendation/src/App.jsx
@@ -1,14 +1,21 @@
 
 import './App.css'
 import { MovieCards } from './components/MovieCards'
+import { Favorite } from './pages/Favorite'
 import { Home } from './pages/Home'
+import {Routes, Route} from 'react-router-dom'
+
 function App() {
 
 
   return (
-     <div>
-         <Home/>
-     </div>
+     <main className='main-content'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/favorite' element={<Favorite/>}/>
+      </Routes>
+  
+     </main>
   )
 }
 
